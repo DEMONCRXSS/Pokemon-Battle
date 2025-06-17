1bc48c04d4770d70f6963fa1506dab987fc33798
DEMONCRXSS <156143511+DEMONCRXSS@users.noreply.github.com>
2025-06-16 20:45:44 -0600

    Attack

---
+++ Assets/Scripts/AttackData.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
+public class AttackData : ScriptableObject
+{
+    public Attack[] attacks;
+}
+
+[System.Serializable]
+
+public class Attack
+{
+    public string attackNamne;
+
+    public float minDamage;
+
+    public float maxDamage;
+
+    public float attackDuration;
+
+    public string animationName;
+
+    public string soundName;
+}
---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    [SerializeField]
+
+    private string _fighterName;
+
+    public string FighterName => _fighterName;
+    [SerializeField]
+
+    private AttackData _AttackData;
+
+    public AttackData AttackData => _AttackData;
+    [SerializeField]
+
+    private Health _health;
+
+    public Health Health => _health;
+    [SerializeField]
+
+    private Animator _CharacterAnimator;
+
+    public Animator CharacterAnimator => _CharacterAnimator;
+}
